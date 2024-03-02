5c555295f7573ed4743c2585c38277374200525b
Mehrnaz4Hosseini <mehrnazhosseini4@gmail.com>
2024-03-03 00:11:17 +0330

    add changes to products' models

--- products/models.py
+++ products/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# any change here -> python manage.py makemigrations
+#                    python manage.py migrate
+            
 
 class Product(models.Model):          # parent class -> the class inherits all the attributes from models.Model
-    title       = models.TextField()
-    description = models.TextField()
-    price       = models.TextField()
-    summary     = models.TextField(default = 'Whatever you want.')
+    title       = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True) # blank = True -> we can leave the description empty on site
+                                                          # null = True  -> description in database can be null
+    price       = models.DecimalField(max_digits=10, decimal_places=2)
+    summary     = models.TextField(blank=False, null=False) # summary is required
+    featured    = models.BooleanField(null=True) # null=True / default=value
