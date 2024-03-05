0488599e36e6fb76ab848deb27def35a621a39e6
Mehrnaz4Hosseini <mehrnazhosseini4@gmail.com>
2024-03-05 23:43:55 +0330

    delete serializers.py from mysite folder

--- mysite/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from products.models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:  # metadata describing model
-        model = Product
-        fields = ['title', 'description', 'price', 'summary', 'featured'] # return model throgh API
