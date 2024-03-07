2b29aa3d5815ae71ea24a5b0c037a45cda4c7787
Mehrnaz4Hosseini <mehrnazhosseini4@gmail.com>
2024-03-07 12:31:44 +0330

    add Review model to reviews

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from pages.views import home_view
+from reviews.views import home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# any change here -> python manage.py makemigrations
+#                    python manage.py migrate
+
+class Review(models.Model):
+    user_id = models.IntegerField()
+    product_id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    rating = models.IntegerField()
+    date_posted = models.DateTimeField(auto_now_add=True)
